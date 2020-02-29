0e3963805642638e0e3a347ae4fc4723f8f9d8e6
Leticia Campos <leticia.campos@totvs.com.br>
2020-02-28 23:54:45 -0300

    passando dados entre componentes

--- src/Tabela.js
+++ src/Tabela.js
@@ -13,40 +13,39 @@ const TableHead = () => {
     );
 }
 
-const TableBody = () => {
+const TableBody = props => {
+    const linhas = props.autores.map((linha, index) => {
+        return (
+            <tr>
+                <td>{linha.nome}</td>
+                <td>{linha.livro}</td>
+                <td>{linha.preco}</td>
+                <td><button>Remover</button></td>
+            </tr>
+
+        )
+    });
+
     return(
         <tbody>
-            <tr>
-                <td>Paulo</td>
-                <td>React</td>
-                <td>1000</td>
-                <td><button>Remover</button></td>
-            </tr>
-            <tr>
-                <td>Nico</td>
-                <td>React</td>
-                <td>1000</td>
-                <td><button>Remover</button></td>
-            </tr>
-            <tr>
-                <td>Daniel</td>
-                <td>React</td>
-                <td>1000</td>
-                <td><button>Remover</button></td>
-            </tr>
+            {linhas}
         </tbody>
     );
 }
 
 class Tabela extends Component {
     render() {
+        const { autores } = this.props;
+
         return (
             <table>
-            <TableHead />
-            <TableBody />
+                <TableHead />
+                <TableBody autores={autores} />
             </table>
+
         );
     }
+
 }
 
 export default Tabela;
