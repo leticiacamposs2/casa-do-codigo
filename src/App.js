a4288ae2ecdcdeba6ebcce75ab2e881843219bac
Leticia Campos <leticia.campos@totvs.com.br>
2020-02-28 22:57:44 -0300

    componentizando a tabela, utilizando function component e class component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
 import React from 'react';
+import Tabela from './Tabela';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <table>
-        <thead>
-          <tr>
-            <th>Autores</th>
-            <th>Livros</th>
-            <th>Preços</th>
-            <th>Remover</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Paulo</td>
-            <td>React</td>
-            <td>1000</td>
-            <td><button>Remover</button></td>
-          </tr>
-          <tr>
-            <td>Nico</td>
-            <td>React</td>
-            <td>1000</td>
-            <td><button>Remover</button></td>
-          </tr>
-          <tr>
-            <td>Daniel</td>
-            <td>React</td>
-            <td>1000</td>
-            <td><button>Remover</button></td>
-          </tr>
-        </tbody>
-      </table>
+      <Tabela />
     </div>
   );
 }
