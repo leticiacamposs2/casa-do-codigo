b4e6ab7fe4095aa1fd69e0216c55d0d2d9c2ad64
Leticia Campos <leticia.campos@totvs.com.br>
2020-02-28 22:33:52 -0300

    ciração de tabela no app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,36 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Autores</th>
+            <th>Livros</th>
+            <th>Preços</th>
+            <th>Remover</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Paulo</td>
+            <td>React</td>
+            <td>1000</td>
+            <td><button>Remover</button></td>
+          </tr>
+          <tr>
+            <td>Nico</td>
+            <td>React</td>
+            <td>1000</td>
+            <td><button>Remover</button></td>
+          </tr>
+          <tr>
+            <td>Daniel</td>
+            <td>React</td>
+            <td>1000</td>
+            <td><button>Remover</button></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
