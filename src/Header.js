cc34a4326a71b942dd43cdda9c5776945b03dc2c
Leticia Campos <leticia.campos@totvs.com.br>
2020-03-08 21:42:01 -0300

    atualizando o className do header

--- src/Header.js
+++ src/Header.js
@@ -4,15 +4,14 @@ const Header = () => {
     return (
         <nav>
             <div className="nav-wrapper indigo lighten-2">
-                <a href="/" className="brand-logo">Casa do Código</a>
-                <ul className="right">
-                    <li><a href="/autores">Autores</a></li>
-                    <li><a href="/livros">Livros</a></li>
-                    <li><a href="/sobre">Sobre</a></li>
+                <a href="/" className="brand-logo ml-3">Casa do Código</a>
+                <ul id="nav-mobile" className="right">
+                    <li><a href="/">Autores</a></li>
+                    <li><a href="/">Livros</a></li>
+                    <li><a href="/">Sobre</a></li>
                 </ul>
             </div>
         </nav>
     );
 }
-
 export default Header;
